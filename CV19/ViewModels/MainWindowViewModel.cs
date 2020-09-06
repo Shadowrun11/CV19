2bd2ea75ce4f4dc126d512fa814d2282427f929c
Shadowrun <11shadowrun11@gmail.ru>
2020-09-06 22:28:48 +0300

    Добавляем текстовые данные в модель-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,30 @@
 ﻿using CV19.Infrastructure.Commands.Base;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel: ViewModel
     {
+        #region TestDatePoints: IEnumerable<DatePoint> - Тествый набор данных для визуализации графиков
+
+        /// <summary> DESCRIPTION </summary>
+        private IEnumerable<DataPoint> _TestDatePoints;
+
+        /// <summary> DESCRIPTION </summary>
+        public IEnumerable<DataPoint> TestDatePoints
+        {
+            get => _TestDatePoints;
+            set => Set(ref _TestDatePoints, value);
+        }
+
+        #endregion
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19";
 
@@ -61,6 +79,17 @@ namespace CV19.ViewModels
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+
+            var date_points = new List<DataPoint>((int)(360 / 0.1)); 
+            for(var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(2 * Math.PI * x * to_rad);
+
+                date_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            TestDatePoints = date_points;
         }
     }
 }
