6313bc003db5cb90e5e8c980c5f38bf9c0233ef5
Shadowrun <11shadowrun11@gmail.ru>
2020-09-06 22:26:56 +0300

    Добавляем модели

---
+++ CV19/Models/CountryInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models
+{
+
+    internal class CounryInfo : PlaceInfo
+    {
+        public IEnumerable<PlaceInfo> ProvinceCounts { get; set; }
+    }
+
+}
---
+++ CV19/Models/DataPoint.cs
@@ -0,0 +1,11 @@
+﻿namespace CV19.Models
+{
+    internal struct DataPoint
+    {
+        public double XValue { get; set; }
+
+        public double YValue { get; set; }
+
+    }
+
+}
