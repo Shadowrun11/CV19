0444be62f37660629756c7ec7a5da48138ad9342
Shadowrun <11shadowrun11@gmail.ru>
2020-09-21 21:27:19 +0300

    Реализация фильтрации груп студентов в разметке в кода файла обработчиков событий

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.Models.Decanat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,26 @@ namespace CV19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollectionFilter(object sender, FilterEventArgs e)
+        {
+
+            if (!(e.Item is Group group )) return;
+            if (group.Name is null) return;
+            var filter_text = GroupNameFilterText.Text;
+            if (filter_text.Length == 0) return;
+
+            if (group.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (group.Description != null && group.Description.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            e.Accepted = false;
+        }
+
+        private void OnGroupsFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
